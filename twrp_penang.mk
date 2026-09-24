31843e5c30262665f68ea3406749eb8e7c679e7f
PrismaticNetwork <emanuelcarlosabreu@outlook.com>
2026-09-24 13:48:59 -0300

    Change PRODUCT_NAME from omni_penang to twrp_penang

--- twrp_penang.mk
+++ twrp_penang.mk
@@ -10,13 +10,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# $(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit from penang device
 $(call inherit-product, device/motorola/penang/device.mk)
 
 PRODUCT_DEVICE := penang
-PRODUCT_NAME := omni_penang
+PRODUCT_NAME := twrp_penang
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := Holi for arm64
 PRODUCT_MANUFACTURER := motorola
