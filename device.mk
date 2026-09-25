566f2f837407f04043ce592f6dba3f2a4d93ede4
PrismaticNetwork <emanuelcarlosabreu@outlook.com>
2026-09-25 01:14:48 -0300

    Modify device.mk for A/B OTA and package updates
    Updated device.mk to configure A/B OTA and product packages. Is touchsecreen.

--- device.mk
+++ device.mk
@@ -5,31 +5,84 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-LOCAL_PATH := device/motorola/penang
+LOCAL_PATH := device/motorola/penang_g
+
+# Shipping level
+PRODUCT_SHIPPING_API_LEVEL := 30
+PRODUCT_TARGET_VNDK_VERSION := 30
+
+# Enable project quotas and casefolding for emulated storage without sdcardfs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
+# Installs gsi keys into ramdisk, to boot a GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Enable virtual A/B OTA
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk) 
+
+# Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
 # A/B
+AB_OTA_UPDATER := true
+TARGET_ENFORCE_AB_OTA_PARTITION_LIST := true
+
+AB_OTA_PARTITIONS += \
+    boot \
+    dtbo \
+    product \
+    system \
+    system_ext \
+    vbmeta \
+    vbmeta_system \
+    vendor \
+    vendor_boot
+
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
-# Boot control HAL
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_vendor=true \
+    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
+    FILESYSTEM_TYPE_vendor=ext4 \
+    POSTINSTALL_OPTIONAL_vendor=true
 
+# Boot control
 PRODUCT_PACKAGES += \
-    bootctrl.holi
+    android.hardware.boot@1.1-impl-qti.recovery \
+    bootctrl.holi.recovery
 
-# PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-#     bootctrl.holi \
-#     libgptutils \
-#    libz \
-#    libcutils
+# Display
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libion \
+    libxml2 \
+    vendor.display.config@1.0 \
+    vendor.display.config@2.0
 
+# fastbootd
 PRODUCT_PACKAGES += \
-    otapreopt_script \
-    cppreopts.sh \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
+
+# Update engine
+PRODUCT_PACKAGES += \
     update_engine \
+    update_engine_sideload \
     update_verifier \
-    update_engine_sideload
+    checkpoint_gc \
+    otapreopt_script
+
+# qcom encrpytion
+PRODUCT_PACKAGES += \
+    qcom_decrypt \
+    qcom_decrypt_fbe
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# Copy modules for depmod
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*.ko,$(LOCAL_PATH)/prebuilt/modules,$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules/1.1)
